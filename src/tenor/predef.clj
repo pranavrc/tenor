13141245c61d0a39d8d8ea7587538041047b1ec5
Pranav Ravichandran <me@onloop.net>
2015-06-12 22:42:03 +0530

    Function to generate time signatures

--- src/tenor/predef.clj
+++ src/tenor/predef.clj
@@ -3,7 +3,7 @@
 (defprotocol NoteEntities
   (note-value [s] "1, 2, 4, 8, 16")
   (pitch-letter [s] "A, B, C, D, E, F, G")
-  (octave [s] "1, 2, 3, 4, 5, 6 etc.)"))
+  (octave [s] "1, 2, 3, 4, 5, 6 etc."))
 
 (defrecord Note [notation]
   NoteEntities
@@ -11,5 +11,13 @@
   (pitch-letter [this] (str (get notation 1)))
   (octave [this] (- (int (get notation 2)) (int \0))))
 
-(defn time-signature [beats note-value]
-  ())
+(defn time-signature [beat-count & [sig]]
+  (cond
+    (> beat-count 3)
+    (let [beats (rand-nth [2 3 4])
+          sig (concat (or sig '()) (range 1 (inc beats)))]
+      (time-signature (- beat-count beats) sig))
+    (and (<= beat-count 3) (> beat-count 0))
+    (let [sig (concat (or sig '()) (range 1 (inc beat-count)))]
+      (time-signature 0 sig))
+    :else sig))
