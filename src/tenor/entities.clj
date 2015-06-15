bd769f528b719e959db168dfe10e430bd1bd1f54
Pranav Ravichandran <me@onloop.net>
2015-06-15 21:16:47 +0530

    Records and protocols for beats and measures

--- src/tenor/entities.clj
+++ src/tenor/entities.clj
@@ -1,6 +1,13 @@
 (ns tenor.entities)
 
-(defrecord Note [note-value pitch-letter octave])
+(defprotocol Entity
+  (repr [this])
+  (generated [this]))
+
+(defrecord Note [note-value pitch-letter octave]
+  Entity
+  (repr [this] (keyword (str pitch-letter octave)))
+)
 
 (defn make-note [notation]
   (let [notation-re #"^(1|2|4|8|16)([A-G])([#b]?)([1-9])$"
@@ -16,10 +23,10 @@
   (cond
     (> note-count 3)
     (let [beat (rand-nth [2 3 4])
-          sig (concat (or sig '()) (range 1 (inc beat)))]
+          sig (conj (or sig '()) beat)]
       (time-signature (- note-count beat) sig))
     (and (<= note-count 3) (> note-count 0))
-    (let [sig (concat (or sig '()) (range 1 (inc note-count)))]
+    (let [sig (conj (or sig '()) note-count)]
       (time-signature 0 sig))
     :else sig))
 
@@ -30,4 +37,10 @@
       (fn [x] (or (= 1 x) (rand-nth [true false])))
       notes)))
 
-(defrecord Measure [note time-signature])
+(defrecord Beat [note-count note-value]
+  Entity
+  (generated [this] (segment-beat note-count note-value)))
+
+(defrecord Measure [note-count note-value]
+  Entity
+  (generated [this] (time-signature note-count)))
