dbb300f292672460fc0356150f8a643d52a7a95b
Pranav Ravichandran <me@onloop.net>
2015-07-01 20:52:03 +0530

    Conjunct and disjunct motion

--- src/tenor/melody.clj
+++ src/tenor/melody.clj
@@ -34,14 +34,22 @@
   "One perfect octave down (Example: C4 to C3)."
   (- 7 degree))
 
-(defn weighted-random-interval-jumps [scale degree]
+(defn weighted-random-interval-jumps [scale degree weights]
   "Generate an interval jump using weighted random selection,
   from the current note until another compatible note is reached.
   The chances of steps, leaps and octave transitions vary."
   (let [movements '(perfect-unison up-step down-step up-leap down-leap up-octave down-octave)
-        current-move (weighted-choose movements '(0.06 0.35 0.35 0.08 0.08 0.04 0.04))
+        current-move (weighted-choose movements weights)
         temp-degree ((resolve current-move) degree)]
     (if (and (> temp-degree 0) (>= (count scale) temp-degree))
       temp-degree
-      (weighted-random-interval-jumps scale temp-degree))))
+      (weighted-random-interval-jumps scale temp-degree weights))))
+
+(defn conjunct-motion [scale degree]
+  "Melodic motion where steps are more likely to occur than leaps."
+  (weighted-random-interval-jumps scale degree '(0.06 0.35 0.35 0.08 0.08 0.04 0.04)))
+
+(defn disjunct-motion [scale degree]
+  "Melodic motion where leaps are more likely to occur than steps."
+  (weighted-random-interval-jumps scale degree '(0.1 0.1 0.1 0.3 0.3 0.05 0.05)))
 
