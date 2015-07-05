eaa77f06ca4720519d67fdcf319499f814cfcd64
Pranav Ravichandran <me@onloop.net>
2015-07-05 17:45:52 +0530

    Remove obsolete deps from project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject tenor "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.1"]
-                 [clojure-nltk "2.0.3-clj-0"]
-                 [clj-wordnet "0.1.0"]])
+                 [overtone "0.9.1"]])
